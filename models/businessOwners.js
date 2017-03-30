17598573fd2d297ad80e20573673910994a4d401
Genedy <genedymohamed96@gmail.com>
2017-03-30 14:27:51 +0200

    modifying models

--- models/businessOwners.js
+++ models/businessOwners.js
@@ -1,31 +1,34 @@
+//Require dependencies
 var mongoose = require('mongoose');
 
-// Business Owner Schema
+//Business owner Schema
 var BusinessSchema = mongoose.Schema({
     personal_email: { type: String, required: true, unique: true },
     password: String,
     address: String,
     fullname: String,
-    Business_Description: String,
-    Business_emails: [{ email: String, Description: String }],
-    Business_name: { type: String, required: true, unique: true },
-//    Business_logo: img, // we need to look into this 
-    // Services: [{ Service_pic: img,
-    //      Service_name: String, 
-    //      Service_Description: String, 
-    //      Service_price: Number, 
-    //      offer : Numer , // can't read it well from the paper
-    //      Time_flag : Boolean , 
-    //      Available_flag : Boolean , 
-    //      Service_rate : Number ,
-    //      Service_reviews : [{Review : String}] ,
-    //      choices : String // can't read the word above it in the paper
-    //       }] ,
-    Associated_Bank : String ,
-    Business_website : String ,
-    FAQ : String ,
-    Business_reviews_and_reports : [{Review : String , Report : String}],
-    Business_rate : Number
+    business_name: { type: String, required: true, unique: true },
+    business_description: String,
+    business_emails: [{ email: String, Description: String }],  //Business emails with description each
+    business_logo: { data: Buffer, contentType: String },
+    associated_bank : String,   //The bank the business deals with
+    business_website : String,
+    FAQ : String,   
+    business_reviews_and_reports : [{review : String , Report : String}],   //Array of reviews and reports
+    business_rating : [{rating : Number}],
+
+    //Business Services
+    services: [{ 
+        service_pic: { data: Buffer, contentType: String },
+        service_name: String, 
+        service_Description: String, 
+        service_price: Number, 
+        promotion_offer : Number,   //Percentage dicount on service
+        service_rating : [{rating : Number}],   //Array of ratings to get average
+        service_reviews : [{review : String}],   //Array of reviews
+        type_flag : Boolean    //Whether sevice type is time-based (true) or product-based (false)
+    }] 
 });
 
+//Export Schema
 var Business = module.exports = mongoose.model('businesses', BusinessSchema);
