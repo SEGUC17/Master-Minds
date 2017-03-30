c3b3de9e58d3e2bb582c6a2a887eb30d57cc4ba0
GehadAbdElhalim <halimgego@gmail.com>
2017-03-30 18:08:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = require('./app/routes');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var DB_URI = "mongodb://localhost:27017/portfolio";
+var DB_URI = "mongodb://localhost:27017/BreakOut";
 
 var app = express();
 
