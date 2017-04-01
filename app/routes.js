b736b1527fcf4479badc2f2bed950e575a0b3a6b
marwan95 <marwan95@users.noreply.github.com>
2017-04-01 18:47:42 +0200

    Implemented the authentication system using Passport

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,11 @@ var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 var homepageController = require('./controllers/hompageController');
+var authenticationController = require('./authenticate');
 
 //Add routes
 router.get('/', homepageController.test);
-
+router.post('/auth',authenticationController);
+router.get('/auth',authenticationController);
 //Export router
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var router = require('./app/routes');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+//var authenticate = require('./routes/authenticate')(passport);
 var mongoose = require('mongoose');
 var DB_URI = "mongodb://localhost:27017/BreakOut";
 var app = express();
@@ -12,6 +14,10 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(__dirname+ '/public'));
 mongoose.Promise = global.Promise;
 mongoose.connect(DB_URI);
+app.use(passport.initialize());
+app.use(passport.session());
+var initPassport = require('./passport-init');
+initPassport(passport);
 app.use(router);
 
 
