63b486070ace887eb50805e6ba7fd25b50195ce0
Ali-Elbeheiry <ali.el.beheiry111@gmail.com>
2017-04-01 16:04:02 +0200

    Still needs more test and need to know how pictures will work

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,15 @@ var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 var homepageController = require('./controllers/hompageController');
+var viewController = require('./controllers/viewController');
+var profileController = require('./controllers/profileController');
 
 //Add routes
 router.get('/', homepageController.test);
+router.get('/viewbusiness', viewController.viewBusiness);
+router.get('/viewservices', viewController.viewServices);
+router.get('/viewprofile', profileController.viewProfile);
+router.post('/editprofile', profileController.editProfile);
 
 //Export router
 module.exports = router;
