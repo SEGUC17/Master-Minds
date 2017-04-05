525a6b5d4637d695899172e526cd1d74db411c11
GehadAbdElhalim <halimgego@gmail.com>
2017-04-06 01:35:24 +0200

    updated login and register for the client

--- server.js
+++ server.js
@@ -6,19 +6,66 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 var DB_URI = "mongodb://localhost:27017/BreakOut";
 var app = express();
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var path = require('path');
+
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'handlebars');
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
 
 //Configure app
-app.set('view engine', 'ejs');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(__dirname+ '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
 mongoose.Promise = global.Promise;
 mongoose.connect(DB_URI);
 app.use(router);
 
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.client = req.client || null;
+    next();
+});
 
 //Start the server
-app.listen(8080, function(){
+app.listen(8080, function() {
     console.log("server is listening on port 8080");
 })
-
