f6af0ffd84ad88f063db28081c6771baa7534079
marwan95 <marwan95@users.noreply.github.com>
2017-04-06 20:25:33 +0200

    Comparing with dev branch.

--- models/businessOwners.js
+++ models/businessOwners.js
@@ -1,35 +1,48 @@
 //Require dependencies
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 //Business owner Schema
 var BusinessSchema = mongoose.Schema({
     personal_email: { type: String, required: true, unique: true },
-    password: String,
+    password: { type: String, required: true },
     address: String,
     fullname: String,
     business_name: { type: String, required: true, unique: true },
     business_description: String,
-    business_emails: [{ email: String, Description: String }],  //Business emails with description each
-    business_logo: { data: Buffer, contentType: String },
-    associated_bank : String,   //The bank the business deals with
-    business_website : String,
-    FAQ : String,   
-    business_reviews_and_reports : [{review : String , Report : String}],   //Array of reviews and reports
-    business_rating : [{rating : Number}],
-    ban: Boolean,    //Whether the business owner has been banned by an admin or not
+
+    business_emails: [{ email: String }],
+    business_logo: String,
+    associated_bank: String, //The bank the business deals with
+    business_website: String,
+    FAQ: String,
+    business_reviews: [{ review: String, reported: { type: Number, default: 0 } }], //Array of reviews and reports
+    business_rating: [{ rating: Number }],
+    accepted: Boolean, // whether or not the business's application to the directory has been accepted by the admin
+
+
     //Business Services
-    services: [{ 
+    services: [{
         service_pic: { data: Buffer, contentType: String },
-        service_name: String, 
-        service_Description: String, 
-        service_price: Number, 
-        promotion_offer : Number,   //Percentage dicount on service
-        service_rating : [{rating : Number}],   //Array of ratings to get average
-        service_reviews : [{review : String}],   //Array of reviews
-        type_flag : Boolean,    //Whether sevice type is time-based (true) or product-based (false)
-        available_flag : Boolean    //Whether service is available or not
-    }] 
+        service_name: String,
+        service_Description: String,
+        service_price: Number,
+
+        promotion_offer: Number, //Percentage dicount on service
+        service_rating: [{ rating: Number }], //Array of ratings to get average
+        service_reviews: [{ review: String, reported: { type: Number, default: 0 } }], //Array of reviews and corresponding reported number
+        type_flag: Boolean, //Whether sevice type is time-based (true) or product-based (false)
+        available_flag: Boolean, //Whether service is available or not
+    }]
+
 });
 
+BusinessSchema.methods.encryptPassword = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
+};
+
+BusinessSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
 //Export Schema
 var Business = module.exports = mongoose.model('businesses', BusinessSchema);
