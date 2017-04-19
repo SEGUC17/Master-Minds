5333c09194274602e6e80f439f85abe6e217f8cf
amr <amr.khalil@student.guc.edu.eg>
2017-04-19 20:00:22 +0200

    getting res back from the backend

--- public/js/controllers/RateAndReview.js
+++ public/js/controllers/RateAndReview.js
@@ -2,19 +2,56 @@ angular.module('Ang_RateAndReviewCtrl', [])
 
 .controller('RateAndReviewBusinessCtrl',function($scope,$http,$location)
 {
-  $scope.Post_Rate= function(RateData)
+  $scope.Post_Rate_Business= function(RateData)
   {
 
   //    console.log($scope.RateData);
     var str_url = $location.url().split('/');
-    $http.post('/routes/rating/'+str_url[str_url.length-1],$scope.RateData);
+    $http.post('/routes/rating/'+str_url[str_url.length-1],$scope.RateData).then(function(res)
+    {
+      //$scope.responce =res;
+      console.log(res.data);
+    });
+
 
   };
-  $scope.Post_Review= function(ReviewData)
+  $scope.Post_Review_Business= function(ReviewData)
   {
     var str_url = $location.url().split('/');
 
-     $http.post('/routes/reviews/'+str_url[str_url.length-1],$scope.ReviewData);
+     $http.post('/routes/reviews/'+str_url[str_url.length-1],$scope.ReviewData).then(function(res)
+     {
+       //$scope.responce =res;
+       console.log(res.data);
+     });
+
        //  console.log(str_url);
   };
+
+    $scope.Post_Rate_Service= function(RateData)
+    {
+
+    //    console.log($scope.RateData);
+      var str_url = $location.url().split('/');
+      $http.post('/routes/rating/'+str_url[str_url.length-2]+'/'+str_url[str_url.length-1],$scope.RateData)
+      .then(function(res)
+      {
+        //$scope.responce =res;
+        console.log(res.data);
+      });
+
+    };
+    $scope.Post_Review_Service= function(ReviewData)
+    {
+      var str_url = $location.url().split('/');
+
+       $http.post('/routes/reviews/'+str_url[str_url.length-2]+'/'+str_url[str_url.length-1],$scope.ReviewData)
+       .then(function(res)
+       {
+         //$scope.responce =res;
+         console.log(res.data);
+       });
+
+         //  console.log(str_url);
+    };
 });
