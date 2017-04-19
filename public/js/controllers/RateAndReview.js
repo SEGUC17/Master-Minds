27c123a9e898d1d57e5ce7e10a02361792b60226
amr <amr.khalil@student.guc.edu.eg>
2017-04-19 17:28:56 +0200

    reading the url and using it to call functions

--- public/js/controllers/RateAndReview.js
+++ public/js/controllers/RateAndReview.js
@@ -1,17 +1,20 @@
 angular.module('Ang_RateAndReviewCtrl', [])
 
-.controller('RateAndReviewBusinessCtrl',function($scope,$http)
+.controller('RateAndReviewBusinessCtrl',function($scope,$http,$location)
 {
   $scope.Post_Rate= function(RateData)
   {
 
-      console.log($scope.RateData);
+  //    console.log($scope.RateData);
+    var str_url = $location.url().split('/');
+    $http.post('/routes/rating/'+str_url[str_url.length-1],$scope.RateData);
 
   };
   $scope.Post_Review= function(ReviewData)
   {
+    var str_url = $location.url().split('/');
 
-      console.log($scope.ReviewData);
-     $http.post('/routes/reviews/business3',$scope.ReviewData);
+     $http.post('/routes/reviews/'+str_url[str_url.length-1],$scope.ReviewData);
+       //  console.log(str_url);
   };
 });
